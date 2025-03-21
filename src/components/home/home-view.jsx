88b4f5afed75f8b2d43e8981d61a195fed5dc707
Abdul Hameed <abdulhameed@myminfotech.com>
2025-03-21 16:19:58 +0530

    mui implemented

--- src/components/home/home-view.jsx
+++ src/components/home/home-view.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <Button>hi</Button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
