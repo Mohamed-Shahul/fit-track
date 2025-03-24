9efdaba2a072c3ae7491e3691f660bd8e458eca6
Mohamed shahul <mohamedshahul@70gmail.com>
2025-03-24 15:10:37 +0530

    sigup page fix

--- src/routes/protected-routes.jsx
+++ src/routes/protected-routes.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/home/home-view";
-import SignUpView from "../components/sign-up/sign-up-view";
 import CreateWorkoutPlan from "../components/create-workout/create-workout-view";
 import TodaysWorkoutPlan from "../components/todays-workout/todays-workout-view";
 
 const ProtectedRoutes = () => {
   return (
     <Routes>
-      <Route path="/sign-up" element={<SignUpView />} />
       <Route path="/home" element={<Home />} />
       <Route path="/create-workout" element={<CreateWorkoutPlan />} />
       <Route path="/todays-workout" element={<TodaysWorkoutPlan />} />
