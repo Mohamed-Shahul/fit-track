5f590fcffc87f45e4b9dfa14030891785f63119a
Abdul Hameed <abdulhameed@myminfotech.com>
2025-04-14 13:15:59 +0530

    stopwatch common state setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import LogInView from "./components/log-in/log-in-view";
 import ProtectedRoutes from "./routes/protected-routes";
 import PrivateRoute from "./routes/ptivate-routes";
-import { isAuthenticated } from "./utilis/auth";
+import {isAuthenticated} from "./utilis/auth";
 import SignUpView from "./components/sign-up/sign-up-view";
+import MyProvider from "./components/context/my-provider";
 
 function App() {
   return (
     <div className="App">
-      <Routes key={localStorage.getItem("isAuthenticated")}>
-        <Route
-          path="/"
-          element={isAuthenticated() ? <Navigate to="/home" /> : <LogInView />}
-        />
-        <Route path="/sign-up" element={<SignUpView />} />
-        <Route
-          path="/*"
-          element={
-            <PrivateRoute>
-              <ProtectedRoutes />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <MyProvider>
+        <Routes key={localStorage.getItem("isAuthenticated")}>
+          <Route
+            path="/"
+            element={
+              isAuthenticated() ? <Navigate to="/home" /> : <LogInView />
+            }
+          />
+          <Route path="/sign-up" element={<SignUpView />} />
+          <Route
+            path="/*"
+            element={
+              <PrivateRoute>
+                <ProtectedRoutes />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </MyProvider>
     </div>
   );
 }
